02f2d3b94cf7d7dffb57d7450b1d17e48991e2d5
horacv <horacv@gmail.com>
2022-05-24 10:05:49 -0500

    Audio Manager Initial setup

--- JetLagGame/Assets/Audio/Scripts/AudioManager.cs
+++ JetLagGame/Assets/Audio/Scripts/AudioManager.cs
@@ -1,18 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using FMOD.Studio;
+using FMODUnity;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private static AudioManager instance;
+    public AudioBusses audioBusses;
+    public SFX sfx;
+    public Music music;
+
     void Start()
     {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    [Serializable]
+    public class AudioBusses
     {
+        public string masterBus;
+        public string sfxBus;
+        public string musicBus;
+    }
+    
+    [Serializable]
+    public class  SFX
+    {
+        public MainCharacter mainCharacter;
+        public Interactables interactables;
+        public Ambiance ambiance;
         
+        [Serializable]
+        public class MainCharacter
+        {
+            public EventReference footsteps;
+            public EventReference jump;
+        }
+
+        [Serializable]
+        public class Interactables
+        {
+            public EventReference boxDrag;
+        }
+        
+        [Serializable]
+        public class Ambiance
+        {
+            public EventReference ambiance;
+        }
+    }
+
+    [Serializable]
+    public class Music
+    {
+        public EventReference music;
     }
 }
