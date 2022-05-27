6707e2477d5c1f978a3be02268b352fea7f7758e
horacv <horacv@gmail.com>
2022-05-27 12:32:30 -0500

    Script cleanup

--- JetLagGame/Assets/Audio/Scripts/AsyncBankLoader.cs
+++ JetLagGame/Assets/Audio/Scripts/AsyncBankLoader.cs
@@ -1,19 +1,3 @@
-//--------------------------------------------------------------------
-//
-// This is a Unity behaviour script that demonstrates how to build
-// a loading screen that can be used at the start of a game to
-// load both FMOD Studio data and Unity data without blocking the
-// main thread.
-//
-// To make this work properly "Load All Event Data at Initialization"
-// needs to be turned off in the FMOD Settings.
-//
-// This document assumes familiarity with Unity scripting. See
-// https://unity3d.com/learn/tutorials/topics/scripting for resources
-// on learning Unity scripting. 
-//
-//--------------------------------------------------------------------
-
 using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
